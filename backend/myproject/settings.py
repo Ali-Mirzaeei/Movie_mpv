5b5414059a3a7eb1fbb41c4ff0ce45d200601ce8
Ali-Mirzaeei <ali.mirzaeei@gmail.com>
2025-12-24 17:45:51 +0330

    17

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "rest_framework",
-    "submissions",
+    "submissions.apps.SubmissionsConfig",
     "corsheaders",   # اگر CORS لازمه
 ]
 
