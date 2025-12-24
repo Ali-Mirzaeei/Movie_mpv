1ebc414b016b17cde477929da16348e38c5e3735
Ali-Mirzaeei <ali.mirzaeei@gmail.com>
2025-12-24 19:56:43 +0330

    01

--- App.tsx
+++ App.tsx
@@ -228,7 +228,7 @@ const App: React.FC = () => {
     };
 
     try {
-      const response = await fetch("https://movie-mind-ol9e.onrender.com/api/submissions/", {
+      const response = await fetch("https://movie-mind-ol9e.onrender.com/api/submit/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -263,7 +263,7 @@ const App: React.FC = () => {
     };
 
     try {
-      const response = await fetch("https://movie-mind-ol9e.onrender.com/api/submissions/", {
+      const response = await fetch("https://movie-mind-ol9e.onrender.com/api/submit/", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
