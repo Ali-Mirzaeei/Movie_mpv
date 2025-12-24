552bb3f592286864c7cca7e5cbf05e696d63c23a
Ali-Mirzaeei <ali.mirzaeei@gmail.com>
2025-12-24 17:52:26 +0330

    18

--- backend/submissions/management/commands/create_superuser_if_not_exists.py
+++ backend/submissions/management/commands/create_superuser_if_not_exists.py
@@ -3,26 +3,30 @@ from django.contrib.auth import get_user_model
 import os
 
 class Command(BaseCommand):
-    help = "Create superuser if it does not exist"
+    help = "Create or update superuser from env vars"
 
-    def handle(self, *args, **options):
+    def handle(self, *args, **kwargs):
         User = get_user_model()
 
         username = os.getenv("DJANGO_SUPERUSER_USERNAME")
         password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
-        email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL", "")
 
         if not username or not password:
             self.stdout.write("Superuser env vars not set")
             return
 
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(
-                username=username,
-                email=email,
-                password=password
-            )
+        user, created = User.objects.get_or_create(
+            username=username,
+            defaults={"email": email, "is_staff": True, "is_superuser": True},
+        )
+
+        user.is_staff = True
+        user.is_superuser = True
+        user.set_password(password)
+        user.save()
+
+        if created:
             self.stdout.write("Superuser created")
         else:
-            self.stdout.write("Superuser already exists")
-#
+            self.stdout.write("Superuser password updated")
