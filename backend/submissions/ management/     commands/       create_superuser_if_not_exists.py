874cda8613e3469c31fe2f0a70adf88a291762c0
Ali-Mirzaeei <ali.mirzaeei@gmail.com>
2025-12-24 17:00:45 +0330

    14

---
+++ backend/submissions/ management/     commands/       create_superuser_if_not_exists.py
@@ -0,0 +1,27 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+import os
+
+class Command(BaseCommand):
+    help = "Create superuser if it does not exist"
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+
+        username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+        password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+        email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+
+        if not username or not password:
+            self.stdout.write("Superuser env vars not set")
+            return
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password
+            )
+            self.stdout.write("Superuser created")
+        else:
+            self.stdout.write("Superuser already exists")
